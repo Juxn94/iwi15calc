bc420bd108244550ab045a43607c45e743fb6c46
Jürgen Reinisch <juergen.reinisch@edu.campus02.at>
2018-01-13 10:39:30 +0100

    added modulo function

--- stackCalculator/src/main/java/at/edu/c02/calculator/Calculator.java
+++ stackCalculator/src/main/java/at/edu/c02/calculator/Calculator.java
@@ -1,14 +1,16 @@
 package at.edu.c02.calculator;
 
-
 public interface Calculator {
 
-	enum Operation {
-		add, sub, mul, div
+    static void main(String[] args){
+    }
+
+    enum Operation {
+		add, sub, mul, div, mod
 	};
 
 	void push(double value);
-	
+
 	double pop() throws CalculatorException;
 	
 	double perform(Operation op) throws CalculatorException;
